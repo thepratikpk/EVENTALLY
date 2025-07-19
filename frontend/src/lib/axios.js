2a077e24ead992e7fc0e61608907d88b46712951
thepratikpk <thepratikpk@gmail.com>
2025-07-19 23:35:48 +0530

    fix: update axios baseURL to include API versioning

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const API=axios.create({
-    baseURL:import.meta.env.VITE_BACKEND_URL,
+    baseURL:import.meta.env.VITE_BACKEND_URL+'/api/v1',
     withCredentials:true
 })
